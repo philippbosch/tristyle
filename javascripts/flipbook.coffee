c30b6e77be0b44d5b5af2e82da84bb5cf27b1d6a
Philipp Bosch <hello@pb.io>
2011-10-30 14:16:46 +0100

    Add total price.

--- javascripts/flipbook.coffee
+++ javascripts/flipbook.coffee
@@ -1,9 +1,18 @@
 BACKEND_URL = "data.json"
+DECIMAL_SEPARATOR = '.'
 
 document.addEventListener 'touchmove', (event) ->
     event.preventDefault()
     window.scroll 0,0
     return false
+
+priceFormat = (price) ->
+    price = parseFloat price
+    price = Math.round(price*100)/100
+    maj = Math.floor(price)
+    min = String(Math.round((price-maj)*100))
+    min = min + "0" if min.length is 1
+    return "#{maj}#{DECIMAL_SEPARATOR}#{min}"
 
 $.getJSON BACKEND_URL, (data) ->
     for flap in ['top', 'middle', 'bottom']
@@ -30,13 +39,24 @@ $.getJSON BACKEND_URL, (data) ->
             carousel.onFlip ->
                 $activeProduct = $ '.swipeview-active .product', this
                 $priceTag = $ '.price', this
+                $total = $ '#total'
+                currentPrice = parseFloat($priceTag.find('.value').text().replace ',','.')
+                currentTotal = parseFloat($total.find('.value').text().replace ',','.')
                 
-                $priceTag.anim scale: 0, opacity: 0, .2, 'linear', ->
-                    $priceTag.find('.value').text $activeProduct.data 'price'
-                    $priceTag.anim scale: 1, opacity: 1, .5, 'cubic-bezier(.17,.67,.24,1)'
+                $priceTag.anim scale: 0, opacity: 0, (if currentPrice then .2 else 0), 'linear', ->
+                    $priceTag.find('.value').text priceFormat($activeProduct.data 'price')
+                    $priceTag.anim scale: 1, opacity: 1, (if currentPrice then .5 else 0), 'cubic-bezier(.17,.67,.24,1)'
                     
                     # cubic-bezier(.17,.67,.24,2) would be nicer but does not work correctly
                     # on mobile safari (https://bugs.webkit.org/show_bug.cgi?id=45761)
+                
+                setTimeout ->
+                    prices = $('.flap .price .value').pluck 'innerHTML'
+                    newTotal = 0.0
+                    newTotal += parseFloat(p) for p in prices
+                    if newTotal != currentTotal
+                        $total.find('.value').text priceFormat(newTotal)
+                , 250
                 
                 for i in [0..2]
                     upcoming = carousel.masterPages[i].dataset.upcomingPageIndex
--- javascripts/flipbook.js
+++ javascripts/flipbook.js
@@ -1,11 +1,23 @@
 (function() {
-  var BACKEND_URL;
+  var BACKEND_URL, DECIMAL_SEPARATOR, priceFormat;
   BACKEND_URL = "data.json";
+  DECIMAL_SEPARATOR = '.';
   document.addEventListener('touchmove', function(event) {
     event.preventDefault();
     window.scroll(0, 0);
     return false;
   });
+  priceFormat = function(price) {
+    var maj, min;
+    price = parseFloat(price);
+    price = Math.round(price * 100) / 100;
+    maj = Math.floor(price);
+    min = String(Math.round((price - maj) * 100));
+    if (min.length === 1) {
+      min = min + "0";
+    }
+    return "" + maj + DECIMAL_SEPARATOR + min;
+  };
   $.getJSON(BACKEND_URL, function(data) {
     var flap, _i, _len, _ref, _results;
     _ref = ['top', 'middle', 'bottom'];
@@ -31,19 +43,34 @@
           carousel.masterPages[i].appendChild(el);
         }
         carousel.onFlip(function() {
-          var $activeProduct, $priceTag, i, upcoming, _results2;
+          var $activeProduct, $priceTag, $total, currentPrice, currentTotal, i, upcoming, _results2;
           $activeProduct = $('.swipeview-active .product', this);
           $priceTag = $('.price', this);
+          $total = $('#total');
+          currentPrice = parseFloat($priceTag.find('.value').text().replace(',', '.'));
+          currentTotal = parseFloat($total.find('.value').text().replace(',', '.'));
           $priceTag.anim({
             scale: 0,
             opacity: 0
-          }, .2, 'linear', function() {
-            $priceTag.find('.value').text($activeProduct.data('price'));
+          }, (currentPrice ? .2 : 0), 'linear', function() {
+            $priceTag.find('.value').text(priceFormat($activeProduct.data('price')));
             return $priceTag.anim({
               scale: 1,
               opacity: 1
-            }, .5, 'cubic-bezier(.17,.67,.24,1)');
+            }, (currentPrice ? .5 : 0), 'cubic-bezier(.17,.67,.24,1)');
           });
+          setTimeout(function() {
+            var newTotal, p, prices, _k, _len3;
+            prices = $('.flap .price .value').pluck('innerHTML');
+            newTotal = 0.0;
+            for (_k = 0, _len3 = prices.length; _k < _len3; _k++) {
+              p = prices[_k];
+              newTotal += parseFloat(p);
+            }
+            if (newTotal !== currentTotal) {
+              return $total.find('.value').text(priceFormat(newTotal));
+            }
+          }, 250);
           _results2 = [];
           for (i = 0; i <= 2; i++) {
             upcoming = carousel.masterPages[i].dataset.upcomingPageIndex;
